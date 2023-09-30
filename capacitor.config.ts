636fd9f611e523960aec7c87cf7319c8c593db52
Ignifero <ju.penag@duocuc.cl>
2023-09-30 16:01:25 -0300

    Primer commit.

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sms_prank',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
